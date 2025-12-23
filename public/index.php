f87a439120aa59fe18c7231b7dc8ac3e53ced925
mohamedredachakir <chakirm082@gmail.com>
2025-12-23 12:57:01 +0100

    create simple logic rooting in index and put line of all pages

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
 <?php
 
 session_start();
+include __DIR__ . "/../config/db.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$uri = trim($uri , '/');
+
+$routes = [
+    "home" => "home.php",
+    "books" => "books.php",
+    "discrbook" => "discrbook.php",
+    "panier" => "panier.php",
+    "profile" => "profile.php",
+    "signup" => "auth/signup.php",
+    "signin" => "auth/signin.php"
+];
+
+if(array_key_exists($uri,$routes)){
+    require __DIR__ . "/../app/controller/" . $routes[$uri];
+}else{
+    require __DIR__ . "/../views/404.php";
+}
 
